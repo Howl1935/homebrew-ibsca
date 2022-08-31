cc216f940c58b736b8a84580f3f0fa27068c94dc
Travis Hawley <travisphawley@gmail.com>
2022-08-31 09:37:13 -0700

    Update ibsca.rb

--- Formula/ibsca.rb
+++ Formula/ibsca.rb
@@ -1,7 +1,7 @@
 class Ibsca < Formula
   desc "IBSCA-CLI for static code analysis at Ibotta"
   homepage "https://github.com/ibotta/ibsca-cli"
-  url "https://github.com/Ibotta/ibsca-cli/releases/download/v1.1.2/ibsca-macos-x64.tar.gz"
+  url "https://github.com/Ibotta/ibsca-cli/releases/download/v1.1.2/ibsca-macos-x64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
   sha256 "1056092a976af44b651a8bc80d1da89bdd0f3353830c0c0c44866cbe56025f2b"
   version "1.1.2"  
   
