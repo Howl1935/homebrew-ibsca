1bc01e00c35ed6d30836419196d4309501ebb512
Travis Hawley <travisphawley@gmail.com>
2022-08-31 08:46:39 -0700

    Update ibsca.rb

--- Formula/ibsca.rb
+++ Formula/ibsca.rb
@@ -1,6 +1,6 @@
 class Ibsca < Formula
   desc "IBSCA-CLI - static code analysis"
-  homepage "https://github.com/ibotta/ibsca-cli"
+  homepage "https://github.com/howl1935/ibsca-cli"
   url "https://github.com/Howl1935/homebrew-ibsca/releases/tag/v1.1.0"
   sha256 "8be2cc4f6ee46a9b105878c7c9f8e8be715e07c1f37a1ded25a9ed16c9661c1f"
   version "1.1.0"  
